b8441be4c9d11208002c9b339efb4ecb94ace8fd
Martin Pecár <xpecar00@vutbr.cz>
2021-04-29 04:20:36 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cv12App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceReference1.Service1Client sluzba= new ServiceReference1.Service1Client();
+
+            labelVysledek.Content = sluzba.Spocti(Convert.ToDecimal(textBoxOp1.Text), Convert.ToDecimal(textBoxOp2.Text), dropDownOperator.SelectedValue.ToString());
+        }
+    }
+}
